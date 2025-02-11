d73b3a29d8279c28c51a885b18065bfadf7cb7fd
LydiaSorbo <lesorbo@gmail.com>
2025-02-11 10:18:41 -0800

    add

--- lecture/lecture_09_sessions_auth/demo-1-end-sessions/routes/users.js
+++ lecture/lecture_09_sessions_auth/demo-1-end-sessions/routes/users.js
@@ -3,8 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.session);
+  if(req.session.userid) {
+    res.send('here is your user information: '+ req.session.userid)
+  } else {
+    res.send('error: you must be logged in')
+  }
 });
 
+router.post('/login', function(req, res, next) {
+  if (req.body.username == 'les' && req.body.password == 'pw') {
+    req.session.userid = "lydia sorbo"
+    console.log("logged in")
+    res.send('you logged in!')
+  } else {
+    console.log("NOT logged in");
+    res.send('wrong login, try again')
+  }
+})
+
+router.post('/logout', function(req, res, next) {
+  console.log('logging out');
+  req.session.destroy();
+  res.send('you are logged out');
+})
 
 export default router;
