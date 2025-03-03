5381171d367a9111c945c9dbe6aa1ea392abc9ca
wena04 <wena04@uw.edu>
2025-03-02 19:10:17 -0800

    setup lab 9

---
+++ lab/lab_09_react_and_authorization/strategy-1-simple-react-build-folder/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import sessions from "express-session";
+
+import WebAppAuthProvider from "msal-node-wrapper";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.enable("trust proxy");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(
+  sessions({
+    secret:
+      "this is some secret key I am making up 093u4oih54lkndso8y43hewrdskjf",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
+
+const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(
+  authConfig
+);
+
+app.use(authProvider.authenticate());
+
+app.get("/signin", (req, res, next) => {
+  return req.authContext.login({
+    postLoginRedirectUri: "/", // redirect here after login
+  })(req, res, next);
+});
+app.get("/signout", (req, res, next) => {
+  return req.authContext.logout({
+    postLogoutRedirectUri: "/", // redirect here after logout
+  })(req, res, next);
+});
+app.use(authProvider.interactionErrorHandler());
+
+export default app;
