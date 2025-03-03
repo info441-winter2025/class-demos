869d182e6f3ab801d6ff5835096bc55e64c4cf75
wena04 <wena04@uw.edu>
2025-03-02 19:21:15 -0800

    setting up continued

--- lab/lab_09_react_and_authorization/strategy-1-simple-react-build-folder/backend/app.js
+++ lab/lab_09_react_and_authorization/strategy-1-simple-react-build-folder/backend/app.js
@@ -6,6 +6,26 @@ import sessions from "express-session";
 
 import WebAppAuthProvider from "msal-node-wrapper";
 
+const authConfig = {
+  auth: {
+    clientId: "Client ID or Application ID HERE",
+    authority:
+      "https://login.microsoftonline.com/Paste_the_Tenant_directory_ID_Here",
+    clientSecret:
+      "Client or Application secret here (NOT THE 'secret id', but the 'secret value')",
+    redirectUri: "/redirect",
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+      logLevel: 3,
+    },
+  },
+};
+
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
