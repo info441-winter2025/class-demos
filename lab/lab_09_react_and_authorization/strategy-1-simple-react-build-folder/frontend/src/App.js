2e4b1dcdd967f9d2bfbf1729d1acca668883c5a2
wena04 <wena04@uw.edu>
2025-03-04 00:25:55 -0800

    lab 9

--- lab/lab_09_react_and_authorization/strategy-1-simple-react-build-folder/frontend/src/App.js
+++ lab/lab_09_react_and_authorization/strategy-1-simple-react-build-folder/frontend/src/App.js
@@ -13,9 +13,26 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Simple React App</h1>
+      <h1 className="title">Lab 9 User Authentication with React Frontend </h1>
       <Welcome user={user} />
       <Identity onUserFetched={setUser} />
+      <div
+        class="tenor-gif-embed"
+        data-postid="23535143"
+        data-share-method="host"
+        data-aspect-ratio="1.16423"
+        data-width="100%"
+      >
+        <a href="https://tenor.com/view/iu-cute-pretty-beautiful-model-gif-23535143">
+          Iu Cute GIF
+        </a>
+        from <a href="https://tenor.com/search/iu-gifs">Iu GIFs</a>
+      </div>{" "}
+      <script
+        type="text/javascript"
+        async
+        src="https://tenor.com/embed.js"
+      ></script>
     </div>
   );
 }
